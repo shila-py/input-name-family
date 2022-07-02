7cee117f2a2bcc9c78c3f4d09da3780e8181263f
shila-py <101890822+shila-py@users.noreply.github.com>
2022-07-02 17:07:12 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,5 @@
+name = input("please input your name : ")
+family = input("please input your family : ")
+age = int(input("please input your age : "))
+print("hello ", name, family, "\n",
+      "next year you will be :", age + 1, "years-old")
